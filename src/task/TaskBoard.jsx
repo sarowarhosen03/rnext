29d1e25369662c7d97679809f8f332ff60925c78
Sarowar Hossain <98280301+sarowarhosen03@users.noreply.github.com>
2024-01-11 02:20:59 +0600

    fixed issu  #1

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -33,6 +33,7 @@ export default function TaskBoard() {
                     return task;
                 })
             );
+            setTaskToUpdate(null);
         }
 
         setShowAddModal(false);
